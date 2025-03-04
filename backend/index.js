4d0c85bcd6923d6af4fc0b0e5f6a848e8efa810f
Vinay <vinay.viny91@gmail.com>
2025-03-04 14:17:37 +0530

    Learn Hub Production

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,50 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 import bodyParser from 'body-parser';
-import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
+import courseRoute from "./routes/course.route.js";
+import mediaRoute from "./routes/media.route.js";
+import purchaseRoute from "./routes/purchaseCourse.route.js";
+import courseProgressRoute from "./routes/courseProgress.route.js";
 import path from 'path';
 
-//Congigure dotenv
 dotenv.config({});
 
-const PORT = process.env.PORT || 8000;
+// call database connection here
+connectDB();
+
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 const _dirname = path.resolve();
 
-//Middleware
+// default middleware
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(cookieParser());
 
-app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+ 
+// apis
+app.use("/api/v1/media", mediaRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/purchase", purchaseRoute);
+app.use("/api/v1/progress", courseProgressRoute);
+ 
+app.use(express.static(path.join(_dirname,"/frontend/dist"))); 
 app.get('*' , (req,res)=>{
     res.sendFile(path.resolve(_dirname , "frontend" , "dist" , "index.html"));
 })
-app.listen(PORT , () => {
-    console.log("Server Created");
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
 })
+
 
