eb942bb3aca2ad81f09411cad551740ff6f66be3
Vinay <vinay.viny91@gmail.com>
2025-03-04 14:24:11 +0530

    Learn Hub Production

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -53,6 +55,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
